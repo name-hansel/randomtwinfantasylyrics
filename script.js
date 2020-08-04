27b18b5e805e088a5f29045afc48c303b5340dcf
snehal-mane <snehal6mane@gmail.com>
2020-08-04 19:22:39 +0530

    Added track name

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function changelyrics() {
     document.getElementById("lyrics").value = "";
+    document.getElementById("track").value = "";
     var request = new XMLHttpRequest();
-    link = getURL();
-    request.open('GET', link);
+    var x = getURL();
+    console.log(x[1])
+    request.open('GET', x[0]);
     request.onreadystatechange = function () {
         if (this.readyState === 4) {
             var obj = JSON.parse(this.responseText);
@@ -20,6 +22,7 @@ function changelyrics() {
             }
             /*set lyrics*/
             document.getElementById("lyrics").innerHTML = lyrics;
+            document.getElementById("track").innerHTML = x[1];
         }
     };
     request.send();
@@ -36,5 +39,6 @@ songs = ["My Boy (Twin Fantasy)","Beach Life-in-Death", "Stop Smoking (We Love Y
 function getURL() {
     song = songs[getRandomInt(0,9)];
     url = "https://api.lyrics.ovh/v1/Car Seat Headrest/" + song;
-    return url;
+    x = [url,song]
+    return x;
 }
